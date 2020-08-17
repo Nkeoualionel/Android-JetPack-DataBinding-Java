5753e0f6f1739be5261956cbccb118660194a812
lionel <nkeoualionel@gmail.com>
2020-08-17 11:47:19 +0200

    creating user class

--- app/src/main/java/com/lionelnkeoua/databiding/User.java
+++ app/src/main/java/com/lionelnkeoua/databiding/User.java
@@ -2,15 +2,48 @@ package com.lionelnkeoua.databiding;
 
 public class User {
 
-    private String mName;
-    private String mAdress;
-    private String mFollower;
-    private String mFollowing;
+    private String name;
+    private String adress;
+    private String follower;
+    private String following;
 
-    public User(String mName, String mAdress, String mFollower, String mFollowing) {
-        this.mName = mName;
-        this.mAdress = mAdress;
-        this.mFollower = mFollower;
-        this.mFollowing = mFollowing;
+
+    public User(String name, String adress, String follower, String following) {
+        this.name = name;
+        this.adress = adress;
+        this.follower = follower;
+        this.following = following;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getFollower() {
+        return follower;
+    }
+
+    public void setFollower(String follower) {
+        this.follower = follower;
+    }
+
+    public String getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(String following) {
+        this.following = following;
     }
 }
