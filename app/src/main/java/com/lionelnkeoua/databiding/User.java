3d2d9fad69a71bc2e29210a69a73be8ab58b10c9
lionel <nkeoualionel@gmail.com>
2020-08-17 09:53:52 +0200

    user model

---
+++ app/src/main/java/com/lionelnkeoua/databiding/User.java
@@ -0,0 +1,16 @@
+package com.lionelnkeoua.databiding;
+
+public class User {
+
+    private String mName;
+    private String mAdress;
+    private String mFollower;
+    private String mFollowing;
+
+    public User(String mName, String mAdress, String mFollower, String mFollowing) {
+        this.mName = mName;
+        this.mAdress = mAdress;
+        this.mFollower = mFollower;
+        this.mFollowing = mFollowing;
+    }
+}
